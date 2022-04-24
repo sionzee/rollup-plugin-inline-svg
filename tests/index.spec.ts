f05fbb35ff24ef948be380c2ca80cff2bb229bfb
Jan Trzicky <jan@sionzee.cz>
2022-04-24 18:53:13 +0200

    fix(ast): allow svg to be non-root nodes (closes #20)

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -6,7 +6,7 @@ describe('InlineSvg', () => {
     const html = `<title>Title here</title>`
     expect(
       () => SvgProcessor.process(html, {fileName: 'test.svg'})
-    ).toThrowError('rollup-plugin-inline-svg: file test.svg is not a valid svg. The root element is not a svg tag.')
+    ).toThrowError('rollup-plugin-inline-svg: file test.svg is not a valid svg. The \'svg\' node was not found.')
   });
 
   it(`multiple roots`, () => {
@@ -20,7 +20,7 @@ describe('InlineSvg', () => {
     const html = ``
     expect(
       () => SvgProcessor.process(html, {fileName: 'test.svg'})
-    ).toThrowError('rollup-plugin-inline-svg: file test.svg is not a valid svg. The root element is not a svg tag.')
+    ).toThrowError('rollup-plugin-inline-svg: file test.svg is not a valid svg. The \'svg\' node was not found.')
   });
 
   it(`forbidden.tags`, () => {
@@ -63,4 +63,16 @@ describe('InlineSvg', () => {
       ]
     )
   });
+
+  it('svg with metadata', () => {
+    const svg = `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-axis-arrow-info" width="24" height="24" viewBox="0 0 24 24">
+    <path d="truncated" />
+</svg>`
+    SvgProcessor.process(svg, {fileName: 'test.svg'})
+    // process shouldn't throw an error
+  })
 })
